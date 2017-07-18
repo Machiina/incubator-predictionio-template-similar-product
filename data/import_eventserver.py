91895b9a1ba69dd71a0bcf878eb78551a8b4708c
miguelalvagarcia <miguel.alvarez@machiina.com>
2017-07-18 13:48:25 +0200

    Revert "Merge branch 'master' into develop"
    This reverts commit 6a3af87e105a9d5b5aeda24f7e2f81edde424d22.

--- data/import_eventserver.py
+++ data/import_eventserver.py
@@ -1,20 +1,3 @@
-#
-# Licensed to the Apache Software Foundation (ASF) under one or more
-# contributor license agreements.  See the NOTICE file distributed with
-# this work for additional information regarding copyright ownership.
-# The ASF licenses this file to You under the Apache License, Version 2.0
-# (the "License"); you may not use this file except in compliance with
-# the License.  You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
 """
 Import sample data for similar product engine
 """
@@ -53,10 +36,7 @@ def import_events(client):
       entity_type="item",
       entity_id=item_id,
       properties={
-        "categories" : random.sample(categories, random.randint(1, 4)),
-        "title": "title for movie " + item_id,
-        "date": 1935 + random.randint(1, 25),
-        "imdbUrl": "http://imdb.com/fake-url/" + item_id
+        "categories" : random.sample(categories, random.randint(1, 4))
       }
     )
     count += 1
